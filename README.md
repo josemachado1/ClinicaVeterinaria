580f7e7abc8ea279f1c30ff00f897749a1e5756b
José <Jos@JOSE>
2017-04-04 12:27:24 +0100

    criação inicial do Model

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# ClinicaVeterinaria
+﻿# ClinicaVeterinaria
 manipulação de base de dados
+
+- manipulação de base de dados
+- criação, edição e eliminação de registos
+- visualização de registos
+- validação dos dados de registos
---
+++ Vets/Vets/Models/VetsDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vets.Models
+{
+    public class VetsDB : DbContext
+    {
+        //representar a Base de Dados
+        //descrevendo as tabelas que la estao contidas
+
+        //representar o 'construtor' desta classe
+        //identificar onde se encontra a Base de Dados
+        public VetsDB() : base("VetsDBConnection") { }
+
+        //decrever as 'tabelas' que estao na BD
+        public virtual DbSet<Donos > Donos { get; set; }
+
+    }
+}
